0a20eb1cd261bd2e21f3ce53ab0503af0175b8d5
jveneroh <146081127+jveneroh@users.noreply.github.com>
2024-11-13 12:28:33 +0100

    Todavía no es el definitivo

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd %userprofile%
+echo Estamos en la ruta de usuario
+mkdir ASIR,DAM,SMR,DAW,CETI
+mkdir ASIR\DOCS,PRACT,EXAM
+mkdir DAM\DOCS,PRACT,EXAM
+mkdir SMR\DOCS,PRACT,EXAM
+mkdir DAW\DOCS,PRACT,EXAM
+mkdir CETI\DOCS,PRACT,EXAM
+
+echo Ya se han creado las carpetas
