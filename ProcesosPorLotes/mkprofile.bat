abde5cd0b72545ccb99d30faaa224e7a3a446c7e
jveneroh <146081127+jveneroh@users.noreply.github.com>
2024-11-18 09:53:11 +0100

    Add files via upload
    Funciona correctamente

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,16 @@
+@echo off
+rem Script de Prueba
+rem Autor: Jesús Venero Hormigo
+rem Fecha: 18-11-2024
+
+mkdir %userprofile%\asir %userprofile%\dam %userprofile%\daw %userprofile%\ceti %userprofile%\smr
+
+mkdir %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\examen
+
+mkdir %userprofile%\dam\docs %userprofile%\dam\pract %userprofile%\dam\examen
+
+mkdir %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\examen
+
+mkdir %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\examen
+
+mkdir %userprofile%\smr\docs %userprofile%\smr\pract %userprofile%\smr\examen
