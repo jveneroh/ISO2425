2dbfe3f6452c152a22c061d89f76acc55ee50e7f
jveneroh <146081127+jveneroh@users.noreply.github.com>
2025-02-03 20:19:47 +0100

    Add files via upload

---
+++ Scripts/calculadora.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+#
+
+
+clear
+
+contador=-1
+
+if [ $# -ne 2 ]; then
+
+	echo "Debes de introducir dos parametros"
+	exit
+
+fi
+
+if [ -f $1 ]; then
+
+	echo "El archivo ya esta creado"
+	exit
+
+fi
+
+
+while true; do
+
+	contador=$(($contador+1))
+	if [ $contador -eq $2 ]; then
+		echo "Has superado el limite de intentos"
+		echo "Te muestro el resultado del fichero y su ruta"
+		pwd $1
+		cat $1
+		break
+	fi
+	echo "S. Para sumar"
+	echo "R. Para la resta"
+	echo "M. Para la multiplicacion"
+	echo "D. Para la division"
+	echo "X. Finalizar"
+	read -p "Elige entre las anteriores opciones " opciones
+
+
+	case $opciones in
+
+		S)
+			read -p "Dime el primer numero " numero1
+			read -p "Dime el segundo numero " numero2
+			echo $opciones $numero1 $numero2 >> $1
+			res=$(($numero1+$numero2))
+			echo "El resultado de la suma es $res"
+		;;
+
+		R)
+
+			read -p "Dime el primer numero " numero1
+			read -p "Dime el segundo numero " numero2
+			echo $opciones $numero1 $numero2 >> $1
+			res=$(($numero1-$numero2))
+			echo "El resultado de la resta es $res"
+
+		;;
+
+		M)
+
+			read -p "Dime el primero numero " numero1
+			read -p "Dime el segundo numero " numero2
+			echo $opciones $numero1 $numero2 >> $1
+			res=$(($numero1*$numero2))
+			echo "El resultado de la multiplicacion es $res"
+
+		;;
+
+		D)
+
+			read -p "Dime el primero numero " numero1
+			read -p "Dime el segundo numero " numero2
+			echo $opciones $numero1 $numero2 >> $1
+			res=$(($numero1/$numero2))
+			echo "El resultado de la division es $res"
+
+		;;
+
+		X)
+
+			echo "Antes de finalizar te muestro el archivo"
+			pwd $1
+			cat $1
+			break
+
+		;;
+
+		*)
+
+			echo "Opcion no valida"
+			break
+
+		;;
+
+	esac
+
+done
